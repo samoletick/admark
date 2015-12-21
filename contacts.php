9b5824cd5f2a050d94971f8824f34ebfd34fe69c
samoletick <askar.musin@gmail.com>
2015-12-21 20:36:21 +0300

    added new files

---
+++ contacts.php
@@ -0,0 +1,8 @@
+<?
+if (isset ($_POST['messageFF'])) {
+  mail ("help@admark.me",
+        "заполнена контактная форма с ".$_SERVER['HTTP_REFERER'],
+        "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nСообщение: ".$_POST['messageFF']);
+  echo ('<p style="color: green">Ваше сообщение получено, спасибо!</p>');
+}
+?>
