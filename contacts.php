809440ea00223df8b6a3b74aa16f6644403ce049
samoletick <askar.musin@gmail.com>
2015-12-21 21:06:56 +0300

    form added

--- contacts.php
+++ contacts.php
@@ -1,7 +1,7 @@
 <?
 if (isset ($_POST['messageFF'])) {
   mail ("help@admark.me",
-        "заполнена контактная форма с ".$_SERVER['HTTP_REFERER'],
+        "Заполнена контактная форма с ".$_SERVER['HTTP_REFERER'],
         "Имя: ".$_POST['nameFF']."\nEmail: ".$_POST['contactFF']."\nСообщение: ".$_POST['messageFF']);
   echo ('<p style="color: green">Ваше сообщение получено, спасибо!</p>');
 }
