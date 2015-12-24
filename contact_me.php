ae1b2967674a4357b82eb1e928358c36644576b7
samoletick <askar.musin@gmail.com>
2015-12-24 22:15:17 +0300

    added

--- contact_me.php
+++ contact_me.php
@@ -1,12 +1,11 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 // check if fields passed are empty
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "Форма не заполнена!";
+	echo "No arguments Provided!";
 	return false;
    }
 	
@@ -17,11 +16,11 @@ $message = $_POST['message'];
 // create email body and send it	
 $to = 'askar@admark.me'; // put your email
 $email_subject = "Форма обратной связи с сайта:  $name";
-$email_body = "У вас новое сообщение. \n\n".
-				  "Вот детали:\n \nName: $name \n ".
-				  "Email: $email_address\n Сообщение \n $message";
-$headers = "От: help@admark.me\n";
-$headers .= "Ответить: $email_address";	
+$email_body = "У вас новое сообщение с сайта. \n\n".
+				  "Here are the details:\n Name: $name \n ".
+				  "Email: $email_address\n Message \n $message";
+$headers = "From: website@admark.me\n";
+$headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
 ?>
