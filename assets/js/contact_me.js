2a0d9bbec8380bfc81b54ba502b54c2e7f1f6148
samoletick <askar.musin@gmail.com>
2016-01-16 00:23:37 +0300

    changes

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -23,7 +23,7 @@ $(function() {
 	   firstName = name.split(' ').slice(0, -1).join(' ');
          }        
 	 $.ajax({
-                url: "assets/js/contact_me.php",
+                url: "../../contact_me.php",
             	type: "POST",
             	data: {name: name, email: email, message: message},
             	cache: false,
--- contact_me.php
+++ contact_me.php
@@ -16,9 +16,9 @@ $message = $_POST['message'];
 // create email body and send it	
 $to = 'askar@admark.me'; // put your email
 $email_subject = "Форма обратной связи с сайта:  $name";
-$email_body = "У вас новое сообщение с сайта. \n\n".
-				  "Here are the details:\n Name: $name \n ".
-				  "Email: $email_address\n Message \n $message";
+$email_body = "У вас новое сообщение с сайта. \n \n".
+				  "Вот детали:\n \nИмя: $name \n".
+				  "Email: $email_address\nСообщение: \n$message";
 $headers = "From: website@admark.me\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
